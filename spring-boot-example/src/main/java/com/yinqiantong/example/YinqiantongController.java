5bb5c5077dc1e3ed72c7cd7ee7013a86fa0e756a
huzhenjie <huzhenjie.dev@gmail.com>
2018-09-20 20:23:28 +0800

    notify api sign check

--- spring-boot-example/src/main/java/com/yinqiantong/example/YinqiantongController.java
+++ spring-boot-example/src/main/java/com/yinqiantong/example/YinqiantongController.java
@@ -1,11 +1,15 @@
 package com.yinqiantong.example;
 
 import com.yinqiantong.Yinqiantong;
+import com.yinqiantong.example.cros.Cros;
+import com.yinqiantong.model.NotifyRes;
 import com.yinqiantong.model.Options;
 import com.yinqiantong.model.Order;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
@@ -15,20 +19,25 @@ public class YinqiantongController {
     private static final String APP_KEY = "1234567890123456";
     private static final String APP_SECRET = "12345678901234567890123456789012";
 
+    @Cros
     @RequestMapping("/order/create")
-    public Order createOrder(HttpServletRequest request) throws Exception {
+    public Order createOrder(String channel, String platform, String code, String extra) throws Exception {
         // 根据您的逻辑，填好Options的参数
+        int money = 1;
+        String clientIp = "183.48.246.68";
         return Yinqiantong.create(APP_ID, APP_KEY, APP_SECRET)
                 .createOrder(
                         Options.newInstance()
-                                .setChannel("alipay")
-                                .setPlatform("h5")
-                                .setMoney(1)
-                                .setClientIp(request)
+                                .setChannel(channel)
+                                .setPlatform(platform)
+                                .setCode(code)
+                                .setMoney(money)
+                                .setClientIp(clientIp)
                                 .setNotifyUrl("https://yinqiantong.com/test")
                 );
     }
 
+    @Cros
     @RequestMapping("/order/query")
     public Order queryOrder(String outTradeNo) throws Exception {
         // 1，判断您当前数据库的订单信息，如果订单已完成，直接返回
@@ -37,11 +46,20 @@ public class YinqiantongController {
     }
 
     @RequestMapping("/notify")
-    public void orderNotify(@RequestHeader String appid,
-                            @RequestHeader String appkey,
-                            @RequestHeader long ts,
-                            @RequestHeader String sign,
-                            @RequestBody Map<String, Object> data) {
+    public NotifyRes orderNotify(@RequestHeader String appid,
+                           @RequestHeader String appkey,
+                           @RequestHeader long ts,
+                           @RequestHeader String sign,
+                           @RequestBody Map<String, Object> data) {
+        boolean isSignOk = Yinqiantong.create(APP_ID, APP_KEY, APP_SECRET).checkSign(data, ts, sign);
+        if (!isSignOk) {
+            // 签名不正确，有可能是 appid appsecret 跟银钱通发过来的不一致导致，请确认 APP_ID, APP_KEY, APP_SECRET 是否正确
+            return NotifyRes.fail();
+        }
+        // 处理回调的 data 内容
+        // ...
+        //
+        return NotifyRes.success();
     }
 
 }
